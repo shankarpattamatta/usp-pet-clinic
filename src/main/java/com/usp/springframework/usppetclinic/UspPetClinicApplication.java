5ecbf92c9cd4b97e8f09722ef579ebd365ff6f10
Umashankar Pattamatta <umashankar.pattamatta@davidjones.com.au>
2021-07-13 14:48:08 +1000

    Initial Update

---
+++ src/main/java/com/usp/springframework/usppetclinic/UspPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.usp.springframework.usppetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UspPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UspPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usp/springframework/usppetclinic/UspPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usp.springframework.usppetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UspPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
